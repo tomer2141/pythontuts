3bf169a85c0976362b4a78b018a4f1bced8b2622
tomer2141 <tomer.sagiv1@gmail.com>
2019-01-06 22:55:48 +0200

    Fix

--- first.py
+++ first.py
@@ -6,6 +6,7 @@ canvas_height = 600
 numArr = []
 nodeSize = 60
 textSize = 30
+lineSpace = 70
 
 master = Tk()
 
@@ -20,7 +21,6 @@ class Tree:
 
     def setRoot(self, node):
         self.root = node
-        node.printNode(True)
 
     def getRoot(self):
         return self.root
@@ -28,12 +28,19 @@ class Tree:
     def getTree(self):
         return self.root.getBranches()
 
+    def printTree(self):
+        self.root.printNode("root", None)
+
 
 class Node:
     def __init__(self, val):
         self.left = None
         self.right = None
         self.value = val
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
 
     def getNodeVal(self):
         return self.value
@@ -70,16 +77,52 @@ class Node:
             print(str(self.left.value) + "<--")
             self.left.getBranches()
 
-
-    def printNode(self, isRoot):
-        if isRoot:
+    def printNode(self, type, parent):
+        x1 = 0
+        y1 = 0
+        x2 = 0
+        y2 = 0
+        if type == "root":
             print("print root node")
-            x1 = canvas_width / 2 - nodeSize / 2
-            y1 = 10
-            x2 = canvas_width / 2 + nodeSize / 2
-            y2 = nodeSize
-            canvas.create_oval(x1, y1, x2, y2) # x1, y1, x2, y2, options
-            canvas.create_text(x1 + nodeSize / 2, (y2 - y1) / 2 + y1 , text=str(self.value))
+            x1 = int(canvas_width / 2 - nodeSize / 2)
+            y1 = int(10)
+            x2 = int(canvas_width / 2 + nodeSize / 2)
+            y2 = int(nodeSize)
+            self.x1 = x1
+            self.x2 = x2
+            self.y1 = y1
+            self.y2 = y2
+        elif type == "right":
+            x1 = int(parent.x2 + nodeSize / 2)
+            x2 = int(x1 + nodeSize)
+            y1 = int(parent.y2 + lineSpace)
+            y2 = int(y1 + nodeSize)
+            self.x1 = x1
+            self.x2 = x2
+            self.y1 = y1
+            self.y2 = y2
+            # handle right
+        elif type == "left":
+            print("left node" + str(self.value))
+            x1 = int(parent.x1 - nodeSize)
+            x2 = int(x1 + nodeSize)
+            y1 = int(parent.y2 + lineSpace)
+            y2 = int(y1 + nodeSize)
+            self.x1 = x1
+            self.x2 = x2
+            self.y1 = y1
+            self.y2 = y2
+            # handle left
+
+        canvas.create_oval(x1, y1, x2, y2)  # x1, y1, x2, y2, options
+        canvas.create_text(x1 + nodeSize / 2, (y2 - y1)
+                           / 2 + y1, text=str(self.value))
+        if self.right is not None:
+            self.right.printNode("right", self)
+
+        if self.left is not None:
+            self.left.printNode("left", self)
+
 
 tree = Tree()
 
@@ -93,7 +136,7 @@ for i, val in enumerate(numArr):
     else:
         tree.root.setNode(n)
 
+print(numArr)
+tree.printTree()
 
-
-tree.getTree()
 mainloop()
