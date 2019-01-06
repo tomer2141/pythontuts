3fc5bb00ed8a90d984d6f02e8783b2034e3692e6
tomer2141 <tomer.sagiv1@gmail.com>
2019-01-06 20:09:45 +0200

    F

--- first.py
+++ first.py
@@ -1,15 +1,79 @@
 from tkinter import *
+from random import randint
 
-canvas_width = 600
-canvas_height = 600
 
-master = Tk()
+class Tree:
+    def __init__(self):
+        self.root = None
 
-w = Canvas(master, width=canvas_width, height=canvas_height)
+    def setRoot(self, node):
+        self.root = node
 
-w.pack()
+    def getRoot(self):
+        return self.root
 
-oval = w.create_oval(75, 75, 500, 500)
-minline = w.create_line(150, 150, 300, 300)
+    def getTree(self):
+        return self.root.getBranches()
 
-mainloop()
+
+class Node:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.value = val
+
+    def getNodeVal(self):
+        return self.value
+
+    def getNodeLeft(self):
+        return self.left
+
+    def getNodeRight(self):
+        return self.right
+
+    def setNode(self, n):
+        if n.value > self.value:
+            if self.right is None:
+                self.right = n
+            else:
+                self.right.setNode(n)
+
+        if n.value < self.value:
+            if self.left is None:
+                self.left = n
+            else:
+                self.left.setNode(n)
+
+    def getBranches(self):
+        if self.right is None:
+            print("end of right branch")
+        else:
+            print("-->" +)
+
+
+tree = Tree()
+numArr = []
+
+for x in range(10):
+    numArr.append(randint(1, 101))
+
+for i, val in enumerate(numArr):
+    n = Node(val)
+    if i == 0:
+        tree.setRoot(n)
+    else:
+        tree.root.setNode(n)
+
+print(numArr)
+
+tree.getTree()
+#canvas_width = 600
+#canvas_height = 600
+
+#master = Tk()
+
+#w = Canvas(master, width=canvas_width, height=canvas_height)
+
+# w.pack()
+
+# mainloop()
